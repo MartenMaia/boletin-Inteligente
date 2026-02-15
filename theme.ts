a4c7e02c8b6b3d16b15292803c0b7674a233b36d
Marten <marten@example.com>
2026-02-14 22:56:50 -0300

    chore(ui): migrate to MUI v5 basic theme, remove Tailwind deps/files

---
+++ theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#49708a'
+    },
+    secondary: {
+      main: '#88abc2'
+    },
+    background: {
+      default: '#ebf7f8'
+    }
+  }
+})
+
+export default theme
